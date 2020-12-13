0e173f84304cef671e01852d366b3a0dc5eebd49
Laura Cole <lauracole1900@comcast.net>
2020-12-12 18:52:18 -0700

    added files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Confetti from "react-confetti";
+import Birthday from "./components/happy_birthday";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Confetti />
+      <Birthday />
+    </>
+  );
+}
+
+export default App;
